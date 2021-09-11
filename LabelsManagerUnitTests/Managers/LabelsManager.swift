24a079cbbd463c5b374fb360681d005753f091e7
Stanislav Sokolov <stasik_sokolov1899@mail.ru>
2021-09-11 17:53:16 +0300

    init commit

---
+++ LabelsManagerUnitTests/Managers/LabelsManager.swift
@@ -0,0 +1,54 @@
+//
+//  LabelsManager.swift
+//  LabelsManagerUnitTests
+//
+//  Created by Станислав on 11.09.2021.
+//
+
+import Foundation
+
+
+struct LabelsManager {
+    
+    enum Auth: CaseIterable {
+        case button
+        case alert
+        case alertButton
+        case registrationButton
+    }
+    
+    enum Main: CaseIterable {
+
+        case firstButton
+        case secondButton
+        case thirdButton
+        
+        var rawValue: String {
+            switch self {
+            case .firstButton: return "firstButton".localized
+            case .secondButton: return "secondButton".localized
+            case .thirdButton: return "thirdButton".localized
+            }
+        }
+
+    }
+    
+}
+
+extension LabelsManager.Auth {
+    var rawValue: String {
+        switch self {
+        case .alert: return "alert".localized
+        case .alertButton: return "alertButton".localized
+        case .button: return "button".localized
+        case .registrationButton: return "registrationButton".localized
+            
+        }
+    }
+}
+
+extension String {
+    var localized: String {
+        NSLocalizedString(self, comment: "")
+    }
+}
--- LabelsManagerUnitTestsTests/LabelsManagerUnitTestsTests.swift
+++ LabelsManagerUnitTestsTests/LabelsManagerUnitTestsTests.swift
@@ -10,24 +10,21 @@ import XCTest
 
 class LabelsManagerUnitTestsTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func testAuthLabels() throws {
+        LabelsManager.Auth.allCases.forEach { value in
+            print("Testing for \(value)")
+            XCTAssertEqual(value.rawValue.isEmpty, false, "Testing is \(value). Value is empty.")
+            XCTAssertNotEqual(value.rawValue, "\(value)", "Testing is \(value). Not localize.")
         }
     }
-
+    
+    func testMainLabels() throws {
+        LabelsManager.Main.allCases.forEach { value in
+            print("Testing for \(value)")
+            XCTAssertEqual(value.rawValue.isEmpty, false, "Testing is \(value). Value is empty.")
+            XCTAssertNotEqual(value.rawValue, "\(value)", "Testing is \(value). Not localize.")
+        }
+    }
+    
+    
 }
